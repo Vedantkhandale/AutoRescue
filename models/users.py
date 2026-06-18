066039dd7da47c63f967177226aae36bdc899a81
vedantkhandale <vedantkhandale8@gmail.com>
2026-06-18 13:04:08 +0530

    update

--- models/users.py
+++ models/users.py
@@ -6,8 +6,8 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
     mobile = db.Column(db.String(15))
-    password = db.Column(db.String(255), nullable=False)
-    role = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(255))
+    role = db.Column(db.String(20))
