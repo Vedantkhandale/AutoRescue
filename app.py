c5fbe1b4eb9afde18efaaa23ab369d82707e5938
vedantkhandale <vedantkhandale8@gmail.com>
2026-06-18 12:27:12 +0530

    update conection file and user tables

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask
+from config import Config
+from models.user import db
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
-
-@app.route("/login")
-def login():
-    return render_template("login.html")
-
-@app.route("/register")
-def register():
-    return render_template("register.html")
+    return "🚗 AutoRescue V2 Running Successfully"
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
     app.run(debug=True)
--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
     SECRET_KEY = "autorescue123"
 
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/autorescue"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/autorescue_v2"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
