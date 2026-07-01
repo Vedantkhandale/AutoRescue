4ee510b2be4dfe139a6f5a4754b07a1f479c0a14
vedantkhandale <vedantkhandale8@gmail.com>
2026-07-01 12:06:12 +0530

    update modify

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
-class Config:
-    SECRET_KEY = "autorescue123"
+import os
 
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/autorescue_v2"
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'autorescue123')
+
+    # Prefer DATABASE_URL env var (e.g. for production), otherwise use a local SQLite file
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL',
+        'sqlite:///autorescue.db'
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
